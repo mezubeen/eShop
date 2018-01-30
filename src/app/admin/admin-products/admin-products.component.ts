04b7accf32adce3d2e0a95b79ddc3c8842fd67c4
zubeen <zubeen95@gmail.com>
2018-01-31 03:51:39 +0530

    added searching for products in admin panel

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,19 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductSaveService } from '../../product-save.service';
+import { Subscription } from 'rxjs/Subscription';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-product$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  product: {title: string}[];
+  subscription: Subscription;
+  filteredProducts: any[];
 
   constructor(private productService: ProductSaveService) {
-   this.product$ = this.productService.getAll();
+   this.subscription = this.productService.getAll()
+    .subscribe(products => this.filteredProducts = this.product = products);
    }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
+
+  filter(query: string){
+    this.filteredProducts = (query) ?
+      this.product.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+      this.product;
   }
 
 }
