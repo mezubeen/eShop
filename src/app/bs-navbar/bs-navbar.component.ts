ee17196092f52bd9f40c34121938872adcd4eb19
mezubeen <zubeen95@gmail.com>
2018-03-08 16:29:37 +0530

    add to cart refactored

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,7 +1,9 @@
+import { Observable } from 'rxjs/Observable';
 import { AppUser } from './../models/app-user';
 import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
 import { ShoppingCartService } from '../shopping-cart.service';
+import { ShoppingCart } from '../models/shopping-cart';
 
 @Component({
   selector: 'bs-navbar',
@@ -10,7 +12,7 @@ import { ShoppingCartService } from '../shopping-cart.service';
 })
 export class BsNavbarComponent implements OnInit {
   appUser: AppUser;
-  shoppingCartItemCount: number;
+  cart$: Observable<ShoppingCart>;
 
   constructor(private auth: AuthService, private shoppingCartService: ShoppingCartService) { 
 
@@ -18,13 +20,8 @@ export class BsNavbarComponent implements OnInit {
 
   async ngOnInit(){
     this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
-    let cart$ = await this.shoppingCartService.getCart();
-    cart$.subscribe(cart => {
-      this.shoppingCartItemCount = 0;
-      for ( let productId in cart.items ) {
-        this.shoppingCartItemCount += cart.items[productId].quantity;
-      }
-    });
+    this.cart$ = await this.shoppingCartService.getCart();
+
   }
 
   logout(){
--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -3,6 +3,8 @@ import { Product } from './models/product';
 import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
 
 
 @Injectable()
@@ -19,9 +21,10 @@ export class ShoppingCartService {
   }
 
   //For reading an exisitng shopping cart
-  async getCart(): Promise<FirebaseObjectObservable<ShoppingCart>> {
+  async getCart(): Promise<Observable<ShoppingCart>> {
     let cartId = await this.getOrCreateCartId();
-    return this.db.object('/shopping-carts/' + cartId);
+    return this.db.object('/shopping-carts/' + cartId)
+      .map(x => new ShoppingCart(x.items));
   }
 
 
